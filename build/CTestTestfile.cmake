7609f34507e824bedec242116c2fc1a1252125c5
dennis <dennismorits@live.com>
2020-03-09 15:04:44 +0100

    workspace catkin aanmaken

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/dennis/Github/Minor-RVD/src
+# Build directory: /home/dennis/Github/Minor-RVD/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
